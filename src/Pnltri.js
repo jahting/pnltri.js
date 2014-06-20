5d224fa7a3001edca325507f7a98d70726ea4c58
Juergen Ahting <gf@ameco.tv>
2014-06-20 10:24:33 +0200

    Version 1.0

--- src/Pnltri.js
+++ src/Pnltri.js
@@ -2,10 +2,11 @@
  * @author jahting / http://www.ameco.tv/
  *
  *	(Simple) Polygon Near-Linear Triangulation
+ *	  with fast ear-clipping for polygons without holes
  *
  */
  
-var PNLTRI = { REVISION: '0.9' };
+var PNLTRI = { REVISION: '1.0' };
 
 //	#####  Global Constants  #####
 
