a21687003e9888d61e6dce43d478883ebeff2af1
Juergen Ahting <gf@ameco.tv>
2014-07-14 15:11:20 +0200

    removed randomness from benchmark

--- test/TriangulatorBenchmarks.js
+++ test/TriangulatorBenchmarks.js
@@ -2,6 +2,19 @@
  * @author jahting / http://www.ameco.tv/
  */
 
+ 
+/*	fix random number generation */
+	
+var random_seed = 1;
+
+var myRandom = function ( inSeed ) {
+	if ( inSeed ) { random_seed = inSeed }
+	var rand = Math.abs(Math.sin(random_seed++)) * 10000;
+	return	rand - Math.floor(rand);
+}
+
+
+
 var	testData = new PolygonTestdata();
 var perf_test_data;
 
@@ -29,12 +42,20 @@ suite.add('squares_perftest_mid', function() {
 );
 
 suite.add('squares_perftest_max', function() {
+		// 80: 205; 88: 783;
+		// 89: 134, 231, 362, 419, 440, 460, 500, 525, 658, 835, 848, 883, 977
+		// 90: 52, 97, 515
+		myRandom( 52 );			// constant seed
 		perf_test_data = testData.get_polygon_with_holes( 'squares_perftest_max' );
 		myTriangulator.triangulate_polygon( perf_test_data );
 		// free memory
 		myTriangulator.clear_lastData();
 		perf_test_data = null;
-	}, { maxTime: 10 }
+	}, { maxTime: 10,
+		setup: function() {
+			r.f.random = myRandom;
+		}
+	}
 );
 
 // called after each run cycle
